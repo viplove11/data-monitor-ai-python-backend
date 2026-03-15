497c5a0ee459f4b67eed6b4411a5a1353172afec
chirag-diwan <diwanashutosh685@gmail.com>
2026-03-15 18:50:50 +0530

    ADDED A TEST DB INIT ROUTE AND SHOW ROUTE TO AVOID USING MYSQL CLIENT

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
+from fastapi import HTTPException
 from fastapi import FastAPI
 
 from app.api.v1.router import api_router
 from app.core.config import settings
 from app.core.logging import setup_logging
+from app.TEST_DB_API.api import initDatabase
+from app.TEST_DB_API.api import print_db
 from sqlalchemy import create_engine
 
 import os
@@ -27,3 +30,18 @@ app.include_router(api_router, prefix=settings.API_V1_PREFIX)
 def root() -> dict[str, str]:
     return {"message": "hello world"}
 
+
+@app.get("/database/show", tags=["show"])
+def show():
+    tables, err = print_db(engine)
+    if err is not None:
+        raise HTTPException(status_code=500, detail=str(err))
+    return {"tables": tables}
+
+
+@app.get("/database/init-db", tags=["init_database"])
+def init_database():
+    err = initDatabase(engine)
+    if err is not None:
+        raise HTTPException(status_code=500, detail=str(err))
+    return {"status": "ok"}
